0b7f8f803d0b1e358c2cdbf0cc6913a944a2e414
Daniel Jhon <115909645+Ikal-Sama@users.noreply.github.com>
2025-08-22 19:44:52 +0800

    fixed rootpage during build

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/CTA";
-import { recentSessions } from "@/constants";
 import {
   getAllCompanions,
   getRecentSessions,
 } from "@/lib/actions/companion.actions";
 import { getSubjectColor } from "@/lib/utils";
+
+export const dynamic = "force-dynamic";
 
 const Page = async () => {
   const companions = await getAllCompanions({ limit: 3 });
